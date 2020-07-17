75b52a94ffb0c02f426cfd68e6b4a03871446da8
lewissam1227 <68430299+lewissam1227@users.noreply.github.com>
2020-07-17 09:55:03 -0400

    Create daily.sh

---
+++ daily.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Hello Samuel, today is" $(date)
+echo "We are about to update and upgrade your Linux system"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt -y autoremove
+echo "Your Linux system has been updated and upgraded"
+
+curl 0w.nz/fsociety.ascii
+
+echo "Kali Linux: The quieter you are, the more you can hear."
+
+                
